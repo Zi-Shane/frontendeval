4ff5d59ce4d1dbed417d70614672851dd5c1787e
Zi-Shane <shane110319@gmail.com>
2024-10-25 11:54:22 +0800

    fix: build error

--- app/MultiForm/type.ts
+++ app/MultiForm/type.ts
@@ -1,6 +1,6 @@
 export type FormObj = { title: string; description: string; value: string };
 
-// type updateFormFunc = (newFormAnswer: FormObj) => void;
+export type updateFormFunc = (newFormAnswer: FormObj) => void;
 
 // enum FormType {
 //   text = "text",
